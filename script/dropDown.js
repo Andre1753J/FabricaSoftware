e1384acef4368018b5f58577979f1b8c03345f15
Andre1753J <andreorocondo7801@gmail.com>
2024-11-21 17:39:47 -0400

    dropDown

--- script/dropDown.js
+++ script/dropDown.js
@@ -1,7 +1,7 @@
 const dropDown = document.querySelector('.dropDown');
 const lidrop = document.querySelector('.lidrop');
 lidrop.addEventListener('mouseover', () => {
-    dropDown.style.height = '10vh';
+    dropDown.style.height = '15vh';
 })
 
 lidrop.addEventListener('mouseout', () => {
