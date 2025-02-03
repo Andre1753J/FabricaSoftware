512cababf1c40c76f0a504a053b0ce391ed89d42
Andre1753J <andreorocondo@gmail.com>
2025-02-03 16:09:56 -0400

    ;

--- React/src/app/page.js
+++ React/src/app/page.js
@@ -4,8 +4,21 @@ import styles from "./page.module.css"
 export default function Inicial() {
     return (
         <div>
-            
-            
+            <section className={styles.card-pai}>
+                <div className={styles.apresentacao}>
+                    <div className={styles.card-titulo}>
+                        <h1 className={styles.gradient}>PET'S WORLD</h1>
+                    </div>
+                    <div className={styles.card-botoes}>
+                        <button className={styles.botao}>
+                            <a href="FichaCliente.html">Cadastrar</a>
+                        </button>
+                        <button className={styles.botao}>
+                            <a href="LoginUsuario.html">Login</a>
+                        </button>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
