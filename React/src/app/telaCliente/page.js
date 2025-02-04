410a15126f95232ed43be20dda46140b6a87dd0e
JulinBn <otaviojulio131@gmail.com>
2025-02-04 17:06:20 -0400

    up

--- React/src/app/telaCliente/page.js
+++ React/src/app/telaCliente/page.js
@@ -17,16 +17,30 @@ export default function Cliente() {
                     <option value="Femenino">Femenino</option>
                 </select>
             </label>
-                <a className={styles.a}>RG<input placeholder="Digite sua inscrição estadual" type="number"></input></a>
-                <a className={styles.a}>CPF/CNPJ<input placeholder="Digite seu CPF ou CNPJ" type="number"></input></a>
-                <a className={styles.a}>E-mail<input placeholder="Digite seu email" type="email"></input></a>
-                <a className={styles.a}>Bairro<input placeholder="Digite seu bairro" type="txt, number"></input></a>
-                <a className={styles.a}>CEP<input placeholder="Digite o CEP" type="number"></input></a>
-                <a className={styles.a}>Complemento<input placeholder="Complemento da residência" type="txt"></input></a>
-                <a className={styles.a}>Telefone 1<input placeholder="" type="number"></input></a>
-                <a className={styles.a}>Telefone 2<input placeholder="" type="number"></input></a>
-            </label>
-
+            <label className={styles.form3}>
+                <a className={styles.a}>RG<input className={styles.inp} placeholder="Digite sua inscrição estadual" type="number"></input></a>
+                </label>
+                <label className={styles.form4}>
+                <a className={styles.a}>CPF/CNPJ<input className={styles.inp} placeholder="Digite seu CPF ou CNPJ" type="number"></input></a>
+                </label>
+                <label className={styles.form5}>
+                <a className={styles.a}>E-mail<input className={styles.inp} placeholder="Digite seu email" type="email"></input></a>
+                </label>
+                <label className={styles.form6}>
+                <a className={styles.a}>Bairro<input className={styles.inp} placeholder="Digite seu bairro" type="txt, number"></input></a>
+                </label>
+                <label className={styles.form7}>
+                <a className={styles.a}>CEP<input className={styles.inp} placeholder="Digite o CEP" type="number"></input></a>
+                </label>
+                <label className={styles.form8}>
+                <a className={styles.a}>Complemento<input className={styles.inp} placeholder="Complemento da residência" type="txt"></input></a>
+                </label>
+                <label className={styles.form9}>
+                <a className={styles.a}>Telefone 1<input className={styles.inp} placeholder="" type="number"></input></a>
+                </label>
+                <label className={styles.form10}>
+                <a className={styles.a}>Telefone 2<input className={styles.inp} placeholder="" type="number"></input></a>
+                </label>
         </div>
     )
 }
