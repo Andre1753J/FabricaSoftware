adf42107991f3eeb6c4655f20e2802f0e41d3bdb
brendavic0902 <brendavitoriagomesdematos7@gmail.com>
2025-02-17 15:13:58 -0400

    sobre nos

--- React/src/app/sobreNos/page.js
+++ React/src/app/sobreNos/page.js
@@ -1,29 +1,30 @@
 import styles from './sobrenos.module.css'
 import Image from 'next/image'
 
+
 export default function sobrenos() {
     return (
-            <section id="sobre-nos">
-                <h1>SOBRE NÓS</h1>
-                <div className="membros">
-                    <div className="membro">
-                        <Image src="andre.jpg" alt="André"/> 
-                        <p>André</p>
+            <section className={styles.sobre_nos}>
+                <h1 className={styles.h1}>SOBRE NÓS</h1>
+                <div className={styles.membros}>
+                    <div className={styles.membro}>
+                        <Image src="/andre.jpg" alt="" width={100} height={100}/> 
+                        <p className={styles.integrantes}>André</p>
                     </div>
-                    <div className="membro">
-                        <Image src="julio.jpg" alt="Julio"/>
-                        <p>Julio</p>
+                    <div className={styles.membro}>
+                        <Image src="/julio.jpg" alt="" width={100} height={100}/>
+                        <p className={styles.integrantes}>Julio</p>
                     </div>
-                    <div className="membro">
-                        <Image src="ikarus.jpg" alt="Ikarus"/>
-                        <p>Ikarus</p>
+                    <div className={styles.membro}>
+                        <Image className={styles.img} src="/ikarus.jpg" alt="" width={100} height={100}/>
+                        <p className={styles.integrantes}>Ikarus</p>
                     </div>
-                    <div className="membro">
-                        <Image src="brenda.jpg" alt="Brenda"/>
-                        <p>Brenda</p>
+                    <div className={styles.membro}>
+                        <Image src="/brenda.jpg" alt="" width={100} height={100}/>
+                        <p className={styles.integrantes}>Brenda</p>
                     </div>
                 </div>
-                <p>Estudantes do IFRO (Instituto Federal de Educação, Ciência e Tecnologia de Rondônia), que estão fazendo um site sobre adoção de animais para a matéria de fábrica de software com o intuito de poder ganhar experiência em novas áreas.</p>
+                <p className={styles.parag}>Estudantes do IFRO (Instituto Federal de Educação, Ciência e Tecnologia de Rondônia), que estão fazendo um site sobre adoção de animais para a matéria de fábrica de software com o intuito de poder ganhar experiência em novas áreas.</p>
             </section>
         
             )
