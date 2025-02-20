a1178eba16298c8e92364317e11b7c949758dd8a
Ikaruszinkk <patoikarus7@gmail.com>
2025-02-20 16:52:16 -0400

    aaaa

---
+++ React/src/app/adocao/page.js
@@ -0,0 +1,54 @@
+import styles from './adocao.module.css'
+import Image from 'next/image'
+
+export default function adocao() {
+    return (
+    <section className={styles.pets}>
+        <div className={styles.telaEsquerda}>
+            Filtro
+        </div>
+        <div className={styles.pet}>
+            <Image  src="/images/brawl-stars.png" width={80} height={80} alt="Brawo Stars"/>
+            <p>Brawl Stars</p>
+            <strong>Macho</strong>
+            <span className={styles.adopted}>JÁ ADOTADO</span>
+        </div>
+        <div className={styles.pet}>
+            <Image  src="/images/luna.png" width={80} height={80} alt="Luna"/>
+            <p>Luna</p>
+            <strong>Fêmea</strong>
+            <span className={styles.adopted}>JÁ ADOTADO</span>
+        </div>
+
+        <div className={styles.pet}>
+            <Image  src="/images/mimi.png" width={80} height={80} alt="Macho"/>
+            <p>Mimi</p>
+            <strong>Macho</strong>
+            <span className={styles.disponivel}>DISPONIVEL</span>
+        </div>
+        <div className={styles.pet}>
+            <Image  src="/images/black-opps-II.png" width={80} height={80} alt="Fêmea"/>
+            <p>Black Opps II</p>
+            <strong>Macho</strong>
+            <span className={styles.disponivel}>DISPONIVEL</span>
+        </div>
+        <div className={styles.pet}>
+            <Image  src="/images/rei-da-coitadolandia.png" width={80} height={80} alt="Fêmea"/>
+            <p>Rei da coitadolandia</p>
+            <strong>Macho</strong>
+            <span className={styles.disponivel}>DISPONIVEL</span>
+        </div>
+        <div className={styles.pet}>
+            <Image  src="/images/ze-ruelinha.png" width={80} height={80} alt="Fêmea"/>
+            <p>Ze Ruelinha</p>
+            <strong>Macho</strong>
+            <span className={styles.disponivel}>DISPONIVEL</span>
+        </div>
+        <div className={styles.pet}>
+            <Image  src="/images/buck.png" width={80} height={80} alt="Fêmea"/>
+            <p>Buck</p>
+            <strong>Macho</strong>
+            <span className={styles.disponivel}>DISPONIVEL</span>
+        </div>
+    </section>
+)}
