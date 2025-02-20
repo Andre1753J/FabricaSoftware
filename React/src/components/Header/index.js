263f0fb37504477979da924f6cde0734c2e2c6e5
Andre1753J <andreorocondo7801@gmail.com>
2025-02-19 23:27:35 -0400

    header

--- React/src/components/Header/index.js
+++ React/src/components/Header/index.js
@@ -7,7 +7,7 @@ import styles from "./Header.module.css";
 
 
 export default function Header() {
-    
+
     const [abrir, setAbrir] = useState(false);
 
     function abrirCoisa() {
@@ -16,37 +16,41 @@ export default function Header() {
     }
 
     return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
-                <Image className={styles.img} width={80} height={80} src="/images/Logo Pet shop roxo.png" alt='logo Pets World'/>
-            </div>
-            <nav className={styles.nav}>
-                <ul className={styles.list}>
-                    <li className={styles.list}>
-                        <Link href='/'>Holmes</Link>
-                    </li>
-                    <li className={styles.list}>
-                        <Link href='/sobreNos'>Sobre</Link>
-                    </li>
-                    <li className={styles.list}>
-                        <Link href='/telaCliente'>Cadastro Cliente</Link>
-                    </li>
-                    <li className={styles.list}>
-                        <Link href='/telaLogin'>Login</Link>
-                    </li>
-                    <li className={styles.list}>
-                        <p className={styles.nada} onClick={abrirCoisa}>Serviço</p>
-                        {abrir && <div>
-                            <button><Link href='#'>Criar Ficha Animal</Link></button>
-                            <button><Link href='#'>Tela de Adoção</Link></button>
-                            <button><Link href='#'>Conta</Link></button>
-                            </div>}
-                    </li>
-                </ul>
-            </nav>
-            <div>
-                <Image className={styles.img} width={70} height={70} src="/images/noIcon.png" alt='F4'/>
-            </div>
+        <header>
+            <section className={styles.header}>
+                <div className={styles.logo}>
+                    <Image className={styles.img} width={80} height={80} src="/images/Logo Pet shop roxo.png" alt='logo Pets World' />
+                </div>
+                <nav className={styles.nav}>
+                    <ul className={styles.list}>
+                        <li className={styles.list}>
+                            <Link href='/'>Holmes</Link>
+                        </li>
+                        <li className={styles.list}>
+                            <Link href='/sobreNos'>Sobre</Link>
+                        </li>
+                        <li className={styles.list}>
+                            <Link href='/telaCliente'>Cadastro Cliente</Link>
+                        </li>
+                        <li className={styles.list}>
+                            <Link href='/telaLogin'>Login</Link>
+                        </li>
+                        <li className={styles.list}>
+                            <p className={styles.nada} onClick={abrirCoisa}>Serviço</p>
+                        </li>
+                    </ul>
+                </nav>
+                <div>
+                    <Image className={styles.img} width={70} height={70} src="/images/noIcon.png" alt='F4' />
+                </div>
+            </section>
+            <section>
+                {abrir && <div className={styles.divNHeader}>
+                    <button className={styles.dropBotao}><Link href='#'>Criar Ficha Animal</Link></button>
+                    <button className={styles.dropBotao}><Link href='#'>Tela de Adoção</Link></button>
+                    <button className={styles.dropBotao}><Link href='#'>Conta</Link></button>
+                </div>}
+            </section>
         </header>
     );
 }
