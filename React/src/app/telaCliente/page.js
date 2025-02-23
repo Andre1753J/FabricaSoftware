ef2a67ef63d21c4eadd63815432c7f1ea88462d6
Andre1753J <andreorocondo7801@gmail.com>
2025-02-23 17:55:06 -0400

    ajustes

--- React/src/app/telaCliente/page.js
+++ React/src/app/telaCliente/page.js
@@ -1,82 +1,63 @@
-import styles from "./telaCliente.module.css";
-import Image from "next/image";
+import styles from './telaCliente.module.css';
+import Link from 'next/link';
 
 export default function Cliente() {
     return (
         <section className={styles.sct}>
-        <div className={styles.div}>
-            <h1 className={styles.titulo}>Cadastro Cliente</h1>
-
-            <label className={styles.t2}><h4 className={styles.t}></h4></label>
-
-            <label className={styles.formulario}>
-                <div className={styles.form}>
-                <label className={styles.cls}>
-                    <p className={styles.plhld1}>NOME</p>
-                    <input className={styles.inpt} placeholder="Digite seu nome" type="txt"></input>
-
-                    <p className={styles.plhld2}>IDADE</p>
-                    <input className={styles.inpt} placeholder="Digite sua idade" type="number"></input> 
-
+            <div className={styles.div}>
+                <h1 className={styles.titulo}>Cadastro Cliente</h1>
+                <label className={styles.t2}><h4 className={styles.t}></h4></label>
+                <label className={styles.formulario}>
+                    <div className={styles.form}>
+                        <label className={styles.cls}>
+                            <p className={styles.plhld1}>NOME</p>
+                            <input className={styles.inpt} placeholder="Digite seu nome" type="text" />
+                            <p className={styles.plhld2}>IDADE</p>
+                            <input className={styles.inpt} placeholder="Digite sua idade" type="number" />
+                        </label>
+                    </div>
+                    <div className={styles.form}>
+                        <label className={styles.cls}>
+                            <p className={styles.plhld3}>SEXO</p>
+                            <select className={styles.gnr}>
+                                <option className={styles.opt}>Masculino</option>
+                                <option className={styles.opt}>Feminino</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div className={styles.form}>
+                        <label className={styles.cls}>
+                            <p className={styles.plhld4}>RG</p>
+                            <input className={styles.inpt} placeholder="Número de RG" type="number" />
+                        </label>
+                    </div>
+                    <div className={styles.form}>
+                        <label className={styles.cls}>
+                            <p className={styles.plhld5}>CPF</p>
+                            <input className={styles.inpt} placeholder="Digite seu CPF" type="number" />
+                            <p className={styles.plhld6}>EMAIL</p>
+                            <input className={styles.inpt} placeholder="Digite seu endereço de email" type="email" />
+                        </label>
+                    </div>
+                    <div className={styles.form}>
+                        <label className={styles.cls}>
+                            <p className={styles.plhld7}>ENDEREÇO</p>
+                            <input className={styles.inpt} placeholder="Digite seu endereço de sua residência" type="text" />
+                            <p className={styles.plhld8}>CEP</p>
+                            <input className={styles.inpt} placeholder="Digite seu código postal" type="number" />
+                        </label>
+                    </div>
+                    <div className={styles.form}>
+                        <label className={styles.cls}>
+                            <p className={styles.plhld9}>PORTE DO ANIMAL</p>
+                            <input className={styles.inpt} placeholder="Complemento do endereço de sua residência" type="text" />
+                        </label>
+                    </div>
                 </label>
-                </div>
-
-                <div className={styles.form}>
-                <label className={styles.cls}>
-               
-                    <p className={styles.plhld3}>SEXO</p>
-                    <select className={styles.gnr}>
-                        <option className={styles.opt}>Masculino</option>
-                        <option className={styles.opt}>Femenino</option>
-                    </select>
-
-                    <p className={styles.plhld4}>RG</p>
-                    <input className={styles.inpt} placeholder="Número de RG" type="number"></input>
-                
-                </label>
-                </div>
-
-                <div className={styles.form}>
-                <label className={styles.cls}>
-
-                    <p className={styles.plhld5}>CPF</p>
-                    <input className={styles.inpt} placeholder="Digite seu CPF" type="number"></input>
-
-                    <p className={styles.plhld6}>EMAIL</p>
-                    <input className={styles.inpt} placeholder="Digite seu endereço de email" type="txt,number"></input>
-
-                </label>
-                </div >
-
-                <div className={styles.form}>
-                <label className={styles.cls}>
-
-                    <p className={styles.plhld7}>ENDEREÇO</p>
-                    <input className={styles.inpt} placeholder="Digite seu endereço de sua residência" type="txt,number"></input>
-
-                    <p className={styles.plhld8}>CEP</p>
-                    <input className={styles.inpt} placeholder="Digite seu código postal" type="number"></input>
-
-                </label>
-                </div >
-
-                <div className={styles.form}>
-                <label className={styles.cls}>
-
-                    <p className={styles.plhld9}>COMPLEMENTO</p>
-                    <input className={styles.inpt} placeholder="Complemento do endereço de sua residência" type=""></input>
-
-                    <p className={styles.plhld10}>TELEFONE 1</p>
-                    <input className={styles.inpt} placeholder="Digite seu telefone" type="number,txt"></input>
-
-                </label>
-                </div >
-
-            </label >
-            <a href="#" className={styles.card_botao}>
-          <button className={styles.button} >Continuar</button>
-        </a>
-        </div >
+                <Link href="/dashboard" className={styles.card_botao}>
+                    <button className={styles.button}>Continuar</button>
+                </Link>
+            </div>
         </section>
-    )
+    );
 }
--- React/src/app/telaLogin/page.js
+++ React/src/app/telaLogin/page.js
@@ -1,30 +1,30 @@
 import styles from './telaLogin.module.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
-export default function telaLogin() {
-  return (
-    <div className={styles.div_pai}>
-      <main className={styles.main}>
-        <h1 className={styles.card_titulo}>Login do Usuário</h1>
-        <section>
-          <label className={styles.email}>E-mail</label>
-          <form className={styles.card_form_1}>
-
-            <input className={styles.inpt_email} type="email" name="text" id="text" placeholder="Digite seu email" required />
-          </form>
-          <form className={styles.card_form_2}>
-            <label className={styles.pass}>Senha</label>
-            <input className={styles.inptpass} type="password" name="pass" id="pass" placeholder="Digite sua senha" required />
-          </form>
-        </section>
-        <a href="#" className={styles.card_botao}>
-          <button className={styles.button} >Continuar</button>
-        </a>
-        <p className={styles.card_texto}>
-          Não tem cadastro?
-          <a href="#" className={styles.card_cada}> Cadastra-se </a>
-        </p>
-      </main>
-    </div>
-  )
+export default function TelaLogin() {
+    return (
+        <div className={styles.div_pai}>
+            <main className={styles.main}>
+                <h1 className={styles.card_titulo}>Login do Usuário</h1>
+                <section>
+                    <label className={styles.email}>E-mail</label>
+                    <form className={styles.card_form_1}>
+                        <input className={styles.inpt_email} type="email" name="text" id="text" placeholder="Digite seu email" required />
+                    </form>
+                    <form className={styles.card_form_2}>
+                        <label className={styles.pass}>Senha</label>
+                        <input className={styles.inptpass} type="password" name="pass" id="pass" placeholder="Digite sua senha" required />
+                    </form>
+                </section>
+                <Link href="/dashboard" className={styles.card_botao}>
+                    <button className={styles.button}>Continuar</button>
+                </Link>
+                <p className={styles.card_texto}>
+                    Não tem cadastro?
+                    <Link href="/cadastro" className={styles.card_cada}> Cadastra-se </Link>
+                </p>
+            </main>
+        </div>
+    );
 }
