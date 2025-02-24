196be6cf91fec769e032b175fbd6645aa43e1023
Andre1753J <andreorocondo7801@gmail.com>
2025-02-24 08:40:38 -0400

    tela de edição

--- React/src/app/telaConta/page.js
+++ React/src/app/telaConta/page.js
@@ -1,7 +1,19 @@
+'use client'
+import React, { useState } from 'react';
 import Image from 'next/image';
 import styles from './telaConta.module.css';
 
 const MinhaConta = () => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className={styles.contaContainer}>
       <h1 className={styles.t1}>Minha Conta</h1>
@@ -23,7 +35,7 @@ const MinhaConta = () => {
           <span className={styles.infoLabel}>Telefone:</span>
           <span className={styles.infoValue}>Você ainda não adicionou um telefone</span>
         </div>
-        <button className={styles.editButton}>Editar perfil de usuário</button>
+        <button className={styles.editButton} onClick={handleEditClick}>Editar perfil de usuário</button>
       </div>
 
       <div className={styles.securitySection}>
@@ -33,6 +45,36 @@ const MinhaConta = () => {
         <button className={styles.deactivateButton}>Desativar Conta</button>
         <button className={styles.logoutButton}>Excluir Conta</button>
       </div>
+
+      {isEditing && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modalContent}>
+            <h2 className={styles.t2}>Editar Perfil</h2>
+            <div className={styles.editItem}>
+              <span className={styles.infoLabel}>Nome exibido:</span>
+              <input type="text" defaultValue="Usuario123" className={styles.editInput} />
+            </div>
+            <div className={styles.editItem}>
+              <span className={styles.infoLabel}>Nome de Usuário:</span>
+              <input type="text" defaultValue="usuario123" className={styles.editInput} />
+            </div>
+            <div className={styles.editItem}>
+              <span className={styles.infoLabel}>E-mail:</span>
+              <input type="email" defaultValue="www.regmail.com" className={styles.editInput} />
+            </div>
+            <div className={styles.editItem}>
+              <span className={styles.infoLabel}>Telefone:</span>
+              <input type="tel" placeholder="Adicionar telefone" className={styles.editInput} />
+            </div>
+            <div className={styles.editItem}>
+              <span className={styles.infoLabel}>Senha Atual:</span>
+              <input type="password" placeholder="********" className={styles.editInput} />
+            </div>
+            <button className={styles.cancelButton} onClick={handleCloseModal}>Cancelar</button>
+            <button className={styles.saveButton}>Pronto</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
