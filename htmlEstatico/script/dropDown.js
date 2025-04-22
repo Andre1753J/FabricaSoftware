599001b24b4cc13b768103d90ece834bd238fc51
Andre1753J <andreorocondo7801@gmail.com>
2025-04-22 16:53:17 -0400

    limpeza

--- htmlEstatico/script/dropDown.js
+++
@@ -1,9 +0,0 @@
-const dropDown = document.querySelector('.dropDown');
-const lidrop = document.querySelector('.lidrop');
-lidrop.addEventListener('mouseover', () => {
-    dropDown.style.height = '22vh';
-})
-
-lidrop.addEventListener('mouseout', () => {
-        dropDown.style.height = '0';
-});
