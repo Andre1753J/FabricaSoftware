280d21cf36a76c3cf73ced1bfdc3ce0a8bf091c8
JulinBn <otaviojulio131@gmail.com>
2025-05-14 14:49:00 -0400

    otimização

--- src/app/pagInfo/page.js
+++ src/app/pagInfo/page.js
@@ -1,16 +1,26 @@
 import styles from './paginfo.module.css';
+import Link from 'next/link'; // Remova se não estiver usando Next.js
 
 export default function PagInfo() {
     return (
         <main className={styles.main}>
-            <div className={styles.card_primeiro}>
-                <p className={styles.card_text_1}>Navegue pelo nosso site e conheça os cães e gatos que esperam ansiosamente por uma família para amar. Você encontrará fotos, histórias e informações detalhadas sobre cada um deles.</p>
-                <h1 className={styles.titulo1}>Bem-Vindos ao<br />Pet’s World!</h1>
-                <a href="/adocao"><button className={styles.adt}>Adote</button></a>
-            </div>
-            <div className={styles.card_segundo}>
-                <p className={styles.card_text_2}>Aqui no Pet's World, acreditamos que todo animal merece um lar cheio de amor. 💖 Somos um site dedicado a conectar animais em busca de um lar com pessoas que sonham em ter um companheiro de quatro patas. 🐾</p>
-            </div>
+            <section className={styles.card_primeiro}>
+                <h1 className={styles.titulo1}>
+                    Bem-Vindos ao <span className={styles.break}>Pet’s World!</span>
+                </h1>
+                <p className={styles.card_text_1}>
+                    Navegue pelo nosso site e conheça os cães e gatos que esperam ansiosamente por uma família para amar.
+                    Você encontrará fotos, histórias e informações detalhadas sobre cada um deles.
+                </p>
+                <Link href="/adocao" className={styles.adt}>Adote</Link>
+            </section>
+
+            <section className={styles.card_segundo}>
+                <p className={styles.card_text_2}>
+                    Aqui no Pet's World, acreditamos que todo animal merece um lar cheio de amor. 💖 Somos um site dedicado 
+                    a conectar animais em busca de um lar com pessoas que sonham em ter um companheiro de quatro patas. 🐾
+                </p>
+            </section>
         </main>
     );
 }
