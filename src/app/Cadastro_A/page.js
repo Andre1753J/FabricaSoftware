f1b2da631cee6f7fb15242b9e6ab936b3a86e769
JulinBn <otaviojulio131@gmail.com>
2025-06-11 16:09:54 -0400

    commit

--- src/app/Cadastro_A/page.js
+++ src/app/Cadastro_A/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from 'react';
 import styles from './cadastro_A.module.css';
@@ -6,14 +6,14 @@ import styles from './cadastro_A.module.css';
 export default function FichaAnimal() {
     const [formData, setFormData] = useState({
         nome: '',
-        raca: '',
         idade: '',
+        sexo: '',
+        raca: '',
         vacinas: '',
         mae: '',
         pai: '',
         situacao: '',
         ultimoVermifugo: '',
-        sexo: '',
         especie: '',
         imagem: null,
     });
@@ -37,141 +37,36 @@ export default function FichaAnimal() {
             <main className={styles.main}>
                 <h1 className={styles.title}>FICHA ANIMAL</h1>
 
-                <div className={styles.title_image_main}>
-                    <label className={styles.title_image}>Foto do Animal</label>
-                </div>
-
-                <div className={styles.box_image}>
-                    <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} className={styles.formFull}>
+                    <div className={styles.formGroup}>
+                        <label className={styles.label} htmlFor="nome">Nome *</label>
                         <input
-                            className={styles.button_img}
-                            type="file"
-                            id="img"
-                            name="imagem"
-                            accept="image/*"
-                            onChange={handleFileChange}
+                            className={styles.input}
+                            type="text"
+                            id="nome"
+                            name="nome"
+                            placeholder="Nome do animal"
+                            value={formData.nome}
+                            onChange={handleChange}
+                            required
                         />
-                    </form>
-                </div>
+                    </div>
 
-                <div className={styles.image_button_main}>
-                    <input className={styles.image_button} type="submit" value="Enviar Imagem" />
-                </div>
+                    <div className={styles.formGroup}>
+                        <label className={styles.label} htmlFor="idade">Data de nascimento *</label>
+                        <input
+                            className={styles.input}
+                            type="date"
+                            id="idade"
+                            name="idade"
+                            value={formData.idade}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
 
-                <div className={styles.formContainer}>
-                    <form className={styles.form} onSubmit={handleSubmit}>
-                        <div className={styles.formGroup}>
-                            <label className={styles.label} htmlFor="nome">Nome</label>
-                            <input
-                                className={styles.input}
-                                type="text"
-                                id="nome"
-                                name="nome"
-                                placeholder="Nome do animal"
-                                value={formData.nome}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-
-                        <div className={styles.formGroup}>
-                            <label className={styles.label} htmlFor="raca">Raça</label>
-                            <input
-                                className={styles.input}
-                                type="text"
-                                id="raca"
-                                name="raca"
-                                placeholder="Raça do animal"
-                                value={formData.raca}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-
-                        <div className={styles.formGroup}>
-                            <label className={styles.label} htmlFor="idade">Data de nascimento</label>
-                            <input
-                                className={styles.input}
-                                type="date"
-                                id="idade"
-                                name="idade"
-                                value={formData.idade}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-
-                        <div className={styles.formGroup}>
-                            <label className={styles.label} htmlFor="vacinas">Vacinas Importantes</label>
-                            <input
-                                className={styles.input}
-                                type="text"
-                                id="vacinas"
-                                name="vacinas"
-                                placeholder="Vacinas Importantes que o animal tomou"
-                                value={formData.vacinas}
-                                onChange={handleChange}
-                            />
-                        </div>
-                    </form>
-
-                    <form className={styles.form} onSubmit={handleSubmit}>
-                        <div className={styles.formGroup}>
-                            <label className={styles.label} htmlFor="mae">Mãe</label>
-                            <input
-                                className={styles.input}
-                                type="text"
-                                id="mae"
-                                name="mae"
-                                placeholder="Mãe do animal"
-                                value={formData.mae}
-                                onChange={handleChange}
-                            />
-                        </div>
-
-                        <div className={styles.formGroup}>
-                            <label className={styles.label} htmlFor="pai">Pai</label>
-                            <input
-                                className={styles.input}
-                                type="text"
-                                id="pai"
-                                name="pai"
-                                placeholder="Pai do animal"
-                                value={formData.pai}
-                                onChange={handleChange}
-                            />
-                        </div>
-
-                        <div className={styles.formGroup}>
-                            <label className={styles.label} htmlFor="situacao">Situação</label>
-                            <input
-                                className={styles.input}
-                                type="text"
-                                id="situacao"
-                                name="situacao"
-                                placeholder="Situação do animal"
-                                value={formData.situacao}
-                                onChange={handleChange}
-                            />
-                        </div>
-
-                        <div className={styles.formGroup}>
-                            <label className={styles.label} htmlFor="ultimoVermifugo">Último Vermífugo</label>
-                            <input
-                                className={styles.input}
-                                type="date"
-                                id="ultimoVermifugo"
-                                name="ultimoVermifugo"
-                                value={formData.ultimoVermifugo}
-                                onChange={handleChange}
-                            />
-                        </div>
-                    </form>
-                </div>
-
-                <div className={styles.checkboxGroup}>
                     <fieldset className={styles.fieldset}>
-                        <legend className={styles.legend}>Sexo</legend>
+                        <legend className={styles.legend}>Sexo *</legend>
                         <div className={styles.checkboxItem}>
                             <input
                                 type="radio"
@@ -180,6 +75,7 @@ export default function FichaAnimal() {
                                 value="macho"
                                 onChange={handleChange}
                                 checked={formData.sexo === 'macho'}
+                                required
                             />
                             <label className={styles.label} htmlFor="macho">Macho</label>
                         </div>
@@ -191,10 +87,88 @@ export default function FichaAnimal() {
                                 value="femea"
                                 onChange={handleChange}
                                 checked={formData.sexo === 'femea'}
+                                required
                             />
                             <label className={styles.label} htmlFor="femea">Fêmea</label>
                         </div>
                     </fieldset>
+
+                    <div className={styles.formGroup}>
+                        <label className={styles.label} htmlFor="raca">Raça</label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            id="raca"
+                            name="raca"
+                            placeholder="Raça do animal"
+                            value={formData.raca}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label className={styles.label} htmlFor="vacinas">Vacinas Importantes</label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            id="vacinas"
+                            name="vacinas"
+                            placeholder="Vacinas importantes"
+                            value={formData.vacinas}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label className={styles.label} htmlFor="mae">Mãe</label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            id="mae"
+                            name="mae"
+                            placeholder="Nome da mãe"
+                            value={formData.mae}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label className={styles.label} htmlFor="pai">Pai</label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            id="pai"
+                            name="pai"
+                            placeholder="Nome do pai"
+                            value={formData.pai}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label className={styles.label} htmlFor="situacao">Situação</label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            id="situacao"
+                            name="situacao"
+                            placeholder="Situação do animal"
+                            value={formData.situacao}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label className={styles.label} htmlFor="ultimoVermifugo">Último Vermífugo</label>
+                        <input
+                            className={styles.input}
+                            type="date"
+                            id="ultimoVermifugo"
+                            name="ultimoVermifugo"
+                            value={formData.ultimoVermifugo}
+                            onChange={handleChange}
+                        />
+                    </div>
 
                     <fieldset className={styles.fieldset}>
                         <legend className={styles.legend}>Espécie</legend>
@@ -221,9 +195,21 @@ export default function FichaAnimal() {
                             <label className={styles.label} htmlFor="canino">Canino</label>
                         </div>
                     </fieldset>
-                </div>
 
-                <button className={styles.button} onClick={handleSubmit}>Salvar</button>
+                    <div className={styles.formGroup}>
+                        <label className={styles.label} htmlFor="imagem">Foto do Animal</label>
+                        <input
+                            className={styles.input}
+                            type="file"
+                            id="imagem"
+                            name="imagem"
+                            accept="image/*"
+                            onChange={handleFileChange}
+                        />
+                    </div>
+
+                    <button className={styles.button} type="submit">Salvar</button>
+                </form>
             </main>
         </section>
     );
